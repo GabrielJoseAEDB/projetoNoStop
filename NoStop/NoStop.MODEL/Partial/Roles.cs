af6718f30e8000e6a047b714097b187f5ac54a0c
Reidner Rocha <reidner.red@hotmail.com>
2019-07-01 16:08:48 -0300

    annotations

---
+++ NoStop/NoStop.MODEL/Partial/Roles.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace NoStop.MODEL
+{
+    [MetadataType(typeof(MD_roles))]
+    public partial class Roles
+    {
+        internal class MD_roles
+        {
+            [DisplayName("Nome da Permissão")]
+            public string Nome { get; set; }
+
+            [DisplayName("Status")]
+            public int IDStatus { get; set; }
+        }
+    }
+}
