2fb1748256e429d5013e49a5132fa640b10f9182
Reidner Rocha <reidner.red@hotmail.com>
2019-07-01 20:44:11 -0300

    Athentication

---
+++ NoStop/NoStop.VIEW/Filters/AutorizaAdmin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NoStop.VIEW.Filters
+{
+    public class AutorizaAdmin : System.Web.Mvc.ActionFilterAttribute, System.Web.Mvc.IActionFilter
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+#pragma warning disable CS0252 // Possível comparação de referência inesperada; o lado esquerdo precisa de conversão
+            if (HttpContext.Current.Session["Role"] == null || HttpContext.Current.Session["Role"] != "admin")
+#pragma warning restore CS0252 // Possível comparação de referência inesperada; o lado esquerdo precisa de conversão
+            {
+                filterContext.Result = new System.Web.Mvc.RedirectToRouteResult(new System.Web.Routing.RouteValueDictionary
+                {
+                    {"Controller","Home"},
+                    {"Action","Erro"}
+                });
+            }
+        }
+    }
+}
